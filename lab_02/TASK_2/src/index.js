88bd9440ecf6cafe05b946ab844fbd212ee114ed
Sunshine-ki <sukocheva.alis@mail.ru>
2020-10-11 14:02:53 +0300

    Разнесено на файлы

--- lab_02/TASK_2/src/index.js
+++ lab_02/TASK_2/src/index.js
@@ -1,126 +1,8 @@
 "use strict";
 
-const fs = require("fs");
+const tasks = require("./tasks");
 
 const express = require("express");
-
-const path = require("path");
-
-const ENCODING = "utf-8"
-
-function LoadPage(app, path, file_name) {
-	app.get(path, (request, response) => {
-		const fileContent = fs.readFileSync(file_name, ENCODING);
-		response.end(fileContent);
-	});
-}
-
-function task1(app) {
-	LoadPage(app, "/compare", "compare.html");
-
-	app.get("/compare/result", (request, response) => {
-		const a = request.query.a;
-		const b = request.query.b;
-		const c = request.query.c;
-
-		const aInt = parseInt(a);
-		const bInt = parseInt(b);
-		const cInt = parseInt(c);
-
-		if (!aInt || !bInt || !cInt) {
-			response.end("Input error!");
-			return;
-		}
-
-		const sInt = Math.max(aInt, bInt, cInt);
-
-		response.end("Maximum number: " + sInt);
-	});
-}
-
-function task2(app) {
-	LoadPage(app, "/array_object", "array_object.html");
-
-	app.get("/array_object/result", (request, response) => {
-		const index = request.query.index;
-		const indexInt = parseInt(index);
-		if (!indexInt) {
-			response.end("Input error!");
-			return;
-		}
-
-		const PATH = path.join(__dirname, "..", "data", "task2.json");
-
-		const array = JSON.parse(fs.readFileSync(PATH));
-
-		if (indexInt < 0 || indexInt > array.length) {
-			response.end("Index input error!");
-			return;
-		}
-
-		response.end("Index = " + indexInt + "\nelement = " + array[indexInt - 1]);
-	});
-}
-
-function task3(app) {
-	LoadPage(app, "/generate_html", "generate_html.html");
-
-	app.get("/generate_html/result", (request, response) => {
-		const field_names = request.query.field_names;
-		const address = request.query.address;
-
-		response.end("field_names = " + field_names);
-	});
-
-}
-
-function task4(app) {
-	LoadPage(app, "/number_array", "number_array.html");
-
-	app.get("/number_array/result", (request, response) => {
-		const a = request.query.a;
-		const b = request.query.b;
-		const c = request.query.c;
-
-		const aInt = parseInt(a);
-		const bInt = parseInt(b);
-		const cInt = parseInt(c);
-
-		if (!aInt || !bInt || !cInt) {
-			response.end("Input error!");
-			return;
-		}
-
-		if (cInt < 0) {
-			response.end("C must be positive!");
-			return;
-		}
-
-		// s = ['aaa','bbb','ccc'].join(''); Это по памяти лучше
-		// let num = cInt;
-		// let result = "";
-		// let step = cInt;
-
-		// while (num < aInt)
-		// 	num += step;
-
-		// while (num <= bInt) {
-		// 	result = result + num + " ";
-		// 	num += step;
-		// }
-
-		let result = "";
-		for (let num = aInt; num <= bInt; num++) {
-			if (!(num % cInt)) {
-				result = result + num + " ";
-			}
-		}
-
-		response.end(result);
-	});
-}
-
-
 
 function main() {
 	const MAIN_FILE_NAME = "index.html"
@@ -128,13 +10,15 @@ function main() {
 	const port = 5015;
 	app.listen(port);
 
-	LoadPage(app, "/", MAIN_FILE_NAME);
+	tasks.LoadPage(app, "/", MAIN_FILE_NAME);
 
-	task1(app);
-	task2(app);
-	task3(app);
-	task4(app);
-
+	// for (let i = 1; i <= 4; i++)
+	// 	tasks['task' + i]()
+	console.log("Begin!")
+	tasks.task1(app);
+	tasks.task2(app);
+	tasks.task3(app);
+	tasks.task4(app);
 }
 
 main();
---
+++ lab_02/TASK_2/src/tasks.js
@@ -0,0 +1,122 @@
+const fs = require("fs");
+
+const express = require("express");
+
+const path = require("path");
+
+const ENCODING = "utf-8"
+
+function LoadPage(app, path, file_name) {
+	app.get(path, (request, response) => {
+		const fileContent = fs.readFileSync(file_name, ENCODING);
+		response.end(fileContent);
+	});
+}
+
+function task1(app) {
+	LoadPage(app, "/compare", "compare.html");
+
+	app.get("/compare/result", (request, response) => {
+		const a = request.query.a;
+		const b = request.query.b;
+		const c = request.query.c;
+
+		const aInt = parseInt(a);
+		const bInt = parseInt(b);
+		const cInt = parseInt(c);
+
+		if (!aInt || !bInt || !cInt) {
+			response.end("Input error!");
+			return;
+		}
+
+		const sInt = Math.max(aInt, bInt, cInt);
+
+		response.end("Maximum number: " + sInt);
+	});
+}
+
+function task2(app) {
+	LoadPage(app, "/array_object", "array_object.html");
+
+	app.get("/array_object/result", (request, response) => {
+		const index = request.query.index;
+		const indexInt = parseInt(index);
+		if (!indexInt) {
+			response.end("Input error!");
+			return;
+		}
+
+		const PATH = path.join(__dirname, "..", "data", "task2.json");
+
+		const array = JSON.parse(fs.readFileSync(PATH));
+
+		if (indexInt < 0 || indexInt > array.length) {
+			response.end("Index input error!");
+			return;
+		}
+
+		response.end("Index = " + indexInt + "\nelement = " + array[indexInt - 1]);
+	});
+}
+
+function task3(app) {
+	LoadPage(app, "/generate_html", "generate_html.html");
+
+	app.get("/generate_html/result", (request, response) => {
+		const field_names = request.query.field_names;
+		const address = request.query.address;
+
+		response.end("field_names = " + field_names);
+	});
+
+}
+
+function task4(app) {
+	LoadPage(app, "/number_array", "number_array.html");
+
+	app.get("/number_array/result", (request, response) => {
+		const a = request.query.a;
+		const b = request.query.b;
+		const c = request.query.c;
+
+		const aInt = parseInt(a);
+		const bInt = parseInt(b);
+		const cInt = parseInt(c);
+
+		if (!aInt || !bInt || !cInt) {
+			response.end("Input error!");
+			return;
+		}
+
+		if (cInt < 0) {
+			response.end("C must be positive!");
+			return;
+		}
+
+		// s = ['aaa','bbb','ccc'].join(''); Это по памяти лучше
+		// let num = cInt;
+		// let result = "";
+		// let step = cInt;
+
+		// while (num < aInt)
+		// 	num += step;
+
+		// while (num <= bInt) {
+		// 	result = result + num + " ";
+		// 	num += step;
+		// }
+
+		let result = "";
+		for (let num = aInt; num <= bInt; num++) {
+			if (!(num % cInt)) {
+				result = result + num + " ";
+			}
+		}
+
+		response.end(result);
+	});
+}
+
+
+module.exports = { task1, task2, task3, task4, LoadPage };
